7df892cf7024a678cb626e971dfc40a03873b988
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-04-24 01:40:34 +0300

    feat: get earthquakes api and pass data to map component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./App.css"
+import { useEffect, useState } from "react"
 import Map from "./Map"
+import axios from "axios"
 
 function App() {
+  const [earthQuakesData, setEarthQuakesData] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetchDataApi()
+  }, [])
+
+  function fetchDataApi() {
+    setIsLoading(true)
+    axios
+      .get(
+        "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2025-04-21&minmagnitude=2"
+      )
+      .then((res) => setEarthQuakesData(res.data.features))
+      .catch((err) =>
+        console.error(`Error fetch Earthquakes API ${err.message}`)
+      )
+      .finally(() => setIsLoading(false))
+  }
+
   return (
     <div className='App'>
-      <Map />
+      {!isLoading ? <Map apiData={earthQuakesData} /> : "Loading..."}
     </div>
   )
 }
