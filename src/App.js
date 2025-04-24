aa2141c2ec9bae85c36148576a1ce08db4b9869c
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-04-24 18:53:51 +0300

    feat: calculate the date 2 days before and modify the api url to attach the date as parameter

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ function App() {
   const [earthQuakesData, setEarthQuakesData] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
 
+  // calculate the date 2 days before
+  let twoDaysBefore = new Date()
+  twoDaysBefore.setDate(twoDaysBefore.getDate() - 2)
+  const formatDate = (date) => date.toISOString().split("T")[0] //format the date in proper format for the API url
+  twoDaysBefore = formatDate(twoDaysBefore)
+
   useEffect(() => {
-    fetchDataApi()
+    fetchDataApi(twoDaysBefore)
   }, [])
 
-  function fetchDataApi() {
+  function fetchDataApi(date) {
     setIsLoading(true)
     axios
       .get(
-        "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2025-04-21&minmagnitude=2"
+        `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${date}&minmagnitude=2`
       )
       .then((res) => setEarthQuakesData(res.data.features))
       .catch((err) =>
