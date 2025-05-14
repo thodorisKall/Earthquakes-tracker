85d7f1099dff58eb1b334ba622d9825c239b6452
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-05-14 23:49:11 +0300

    docs: create Banner component

---
+++ src/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Banner() {
+  return (
+    <div className='banner'>
+      <h2>Explore EarthQuakes around the World with just one click!</h2>
+    </div>
+  )
+}
+
+export default Banner
